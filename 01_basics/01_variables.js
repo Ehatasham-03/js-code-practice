cc897b73f874b6ec053f2c98a0edac9e70141b96
Ehatasham-03 <godgearhussain77@gmail.com>
2024-06-20 18:56:13 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 138111
+let accountEmail ="Eha@gmail.com"
+var accountPassword = "138121"
+accountCity = "Siliguri"
+
+//accountId = 2 //not changbale
+ accountEmail = "hussain@123"
+ accountPassword = "212121"
+ accountCity = "Kolkata"
+
+console.log([accountId , accountEmail,accountPassword, accountCity]);
+
+console.table([accountId , accountEmail,accountPassword, accountCity]); //table format
