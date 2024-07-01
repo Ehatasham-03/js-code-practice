fe6c4046fbf50b3477ba71fad4af21d7467d8b49
Ehatasham-03 <godgearhussain77@gmail.com>
2024-07-01 20:21:06 +0530

    prototypes

---
+++ classesAnsOpps/prototype.js
@@ -0,0 +1,62 @@
+// let myName = "Ehatasham        "
+// let myChannel = "code   "
+
+// console.log(myName.truelength);
+
+
+// let myHeros = ["thor","spiderman"]
+
+// let myPower = {
+//     thor: "hammer",
+//     spiderman: "sling",
+
+//     getSpiderPower: function(){
+//         console.log(`spider power is ${this.spiderman}`);
+//     }
+// }
+
+// Object.prototype.ehatasham = function(){
+//     console.log(`ehatasham is there `);
+// }
+// Array.prototype.heyEha =  function(){
+//     console.log(`ehatasham is there`)
+// }
+
+// // myPower.ehatasham( )
+// // myHeros.ehatasham()
+// // myHeros.heyEha()
+// // myPower.heyEha()---- its not done because mypower is not array
+
+// // inheritance
+// const User = {
+//     name: "chai",
+//     email: "chai@gmail.com"
+// }
+// const Teacher = {
+//     makeVideo: TextTrackCue
+// }
+
+// const TeachingSupport = {
+//     isAvailable:false
+// }
+
+// const TASupport = {
+//     makeAssignment: 'JS assignment',
+//     fullTime : true,
+//     __proto__: TeachingSupport
+// }
+// // Teacher.__proto__ = User
+
+//modle syntax
+// Object.setPrototypeOf(TeachingSupport,Teacher)
+
+let anotherUserName = "ChaiAurCode   "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    // console.log(`${this.name}`);
+    console.log(`true length is ${this.trim().length}`);
+}
+anotherUserName.trueLength()
+
+"Ehatasham   ".trueLength()
